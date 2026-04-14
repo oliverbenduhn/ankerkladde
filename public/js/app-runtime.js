f50f91da022335d1965544931bb0c52f1712acd0
Oliver Benduhn <oliverbenduhn@gmail.com>
2026-04-14 13:21:27 +0000

    Extract app runtime orchestration

---
+++ public/js/app-runtime.js
@@ -0,0 +1,245 @@
+import { createAppUiController } from './app-ui.js';
+import { createHelpersController } from './helpers.js';
+import { createItemsActionsController } from './items-actions.js';
+import { createItemsController } from './items.js';
+import { createItemsViewController } from './items-view.js';
+import { createNavigation } from './navigation.js';
+import { createEditorController } from './editor.js';
+import { createReorderController } from './reorder.js';
+import { createRouter } from './router.js';
+import { createScannerController } from './scanner.js';
+import { createSwipeController } from './swipe.js';
+import { createTabsViewController } from './tabs-view.js';
+import {
+    BARCODE_FORMATS,
+    SCANNER_COOLDOWN_MS,
+    getCurrentCategory,
+    getTypeConfig,
+    normalizePreferences,
+    scannerState,
+    state,
+} from './state.js';
+import { applyThemePreferences } from './theme.js';
+import { settingsFrameEl } from './ui.js';
+
+export function createAppRuntime(deps) {
+    const {
+        getNoteSaveTimer,
+        getUserPreferences,
+        getTiptapEditor,
+        setNoteSaveTimer,
+        setTiptapEditor,
+        setUserPreferences,
+    } = deps;
+
+    const appUiController = createAppUiController();
+
+    const updateFilePickerLabel = () => appUiController.updateFilePickerLabel();
+    const helpersController = createHelpersController({
+        getUserPreferences,
+        updateFilePickerLabel,
+    });
+
+    let navigation = null;
+    let router = null;
+    let itemsController = null;
+    let itemsViewController = null;
+    let itemsActionsController = null;
+    let scannerController = null;
+    let editorController = null;
+    let reorderController = null;
+    let swipeController = null;
+    let tabsViewController = null;
+
+    const getItemById = id => itemsController.getItemById(id);
+    const getVisibleCategories = () => itemsController.getVisibleCategories();
+    const cacheCurrentCategoryItems = () => itemsController.cacheCurrentCategoryItems();
+    const invalidateCategoryCache = categoryId => itemsController.invalidateCategoryCache(categoryId);
+    const loadCategories = async () => { await itemsController.loadCategories(); };
+    const savePreferences = async patch => { await itemsController.savePreferences(patch); };
+    const renderCategoryTabs = () => tabsViewController.renderCategoryTabs();
+    const closeScanner = () => scannerController.closeScanner();
+    const handleScannedBarcode = async rawValue => { await scannerController.handleScannedBarcode(rawValue); };
+    const openScanner = async (action = state.mode === 'einkaufen' ? 'toggle' : 'add') => { await scannerController.openScanner(action); };
+    const setCategory = async categoryId => { await itemsController.setCategory(categoryId); };
+    const loadItems = async (categoryId = state.categoryId, options = {}) => { await itemsController.loadItems(categoryId, options); };
+    const prefetchAdjacentCategories = () => itemsController.prefetchAdjacentCategories();
+    const getVisibleItems = () => itemsController.getVisibleItems();
+    const openSearch = () => itemsController.openSearch();
+    const closeSearch = () => itemsController.closeSearch();
+    const doSearch = async query => { await itemsController.doSearch(query); };
+    const renderItems = () => itemsViewController.renderItems();
+    const openNoteEditor = async item => { await editorController.openNoteEditor(item); };
+    const openNoteEditorWithNavigation = async item => { await editorController.openNoteEditorWithNavigation(item); };
+    const closeNoteEditor = async () => { await editorController.closeNoteEditor(); };
+    const scheduleNoteSave = () => editorController.scheduleNoteSave();
+    const resetItemForm = () => helpersController.resetItemForm();
+    const syncSettingsFrameTheme = () => helpersController.syncSettingsFrameTheme(settingsFrameEl);
+    const triggerHapticFeedback = () => helpersController.triggerHapticFeedback();
+    const isOverdueItem = item => helpersController.isOverdueItem(item);
+    const formatDate = value => helpersController.formatDate(value);
+    const setMessage = (text, isError = false) => appUiController.setMessage(text, isError);
+    const setUploadProgress = fraction => appUiController.setUploadProgress(fraction);
+    const makeUploadProgressCallback = () => appUiController.makeUploadProgressCallback();
+    const updateHeaders = () => appUiController.updateHeaders();
+    const updateUploadUi = () => appUiController.updateUploadUi();
+    const setScannerStatus = (text, isError = false) => appUiController.setScannerStatus(text, isError);
+    const setNetworkStatus = () => appUiController.setNetworkStatus();
+    const applyTabsVisibility = hidden => appUiController.applyTabsVisibility(hidden);
+    const formatBytes = sizeBytes => appUiController.formatBytes(sizeBytes);
+
+    router = createRouter({
+        closeNoteEditor,
+        closeScanner,
+        closeSearch,
+        doSearch,
+        getItemById,
+        openNoteEditor,
+        openScanner,
+        openSearch,
+        scannerState,
+        setCategory,
+        updateHeaders,
+    });
+
+    navigation = createNavigation({
+        applyRouteState: router.applyRouteState,
+        getCurrentRouteState: router.getCurrentRouteState,
+    });
+
+    itemsActionsController = createItemsActionsController({
+        cacheCurrentCategoryItems,
+        closeNoteEditor,
+        getItemById,
+        getVisibleCategories,
+        invalidateCategoryCache,
+        loadItems,
+        makeUploadProgressCallback,
+        openNoteEditorWithNavigation,
+        renderItems,
+        resetItemForm,
+        setCategory,
+        setMessage,
+    });
+
+    tabsViewController = createTabsViewController({
+        getTypeConfig,
+        getVisibleCategories,
+        isTabDragJustFinished: () => reorderController?.wasTabDragJustFinished() ?? false,
+        onCategorySelect: setCategory,
+    });
+
+    itemsViewController = createItemsViewController({
+        closeSearch,
+        formatBytes,
+        formatDate,
+        getItemById,
+        getVisibleItems,
+        handleDelete: async id => { await itemsActionsController.handleDelete(id); },
+        handleEditSave: async id => { await itemsActionsController.handleEditSave(id); },
+        handlePin: async (id, isPinned) => { await itemsActionsController.handlePin(id, isPinned); },
+        handleToggle: async (id, done) => { await itemsActionsController.handleToggle(id, done); },
+        isOverdueItem,
+        openNoteEditorWithNavigation,
+        setCategory,
+    });
+
+    itemsController = createItemsController({
+        applyTabsVisibility,
+        applyThemePreferences,
+        closeNoteEditor,
+        closeScanner,
+        closeSettings: () => router.closeSettings(),
+        getUserPreferences,
+        navigation,
+        normalizePreferences,
+        renderCategoryTabs,
+        renderItems,
+        scannerState,
+        setMessage,
+        setUserPreferences,
+        updateHeaders,
+        updateUploadUi,
+    });
+
+    scannerController = createScannerController({
+        getCurrentCategory,
+        getItemById,
+        getScannerCooldownMs: () => SCANNER_COOLDOWN_MS,
+        getScannerSupportedFormats: () => BARCODE_FORMATS,
+        handleToggle: async (id, done) => { await itemsActionsController.handleToggle(id, done); },
+        invalidateCategoryCache,
+        loadItems,
+        navigation,
+        setMessage,
+        setScannerStatus,
+        triggerHapticFeedback,
+        updateFilePickerLabel,
+    });
+
+    editorController = createEditorController({
+        cacheCurrentCategoryItems,
+        getItemById,
+        getNoteSaveTimer,
+        navigation,
+        setNoteSaveTimer,
+        setTiptapEditor,
+        getTiptapEditor,
+    });
+
+    reorderController = createReorderController({
+        applyTabsVisibility,
+        cacheCurrentCategoryItems,
+        getItemById,
+        getUserPreferences,
+        getVisibleCategories,
+        invalidateCategoryCache,
+        loadCategories,
+        loadItems,
+        renderCategoryTabs,
+        setMessage,
+        triggerHapticFeedback,
+        updateHeaders,
+    });
+
+    swipeController = createSwipeController({
+        getUserPreferences,
+        getVisibleCategories,
+        setCategory,
+    });
+
+    return {
+        addItem: async event => { await itemsActionsController.addItem(event); },
+        applyTabsVisibility,
+        clearDone: async () => { await itemsActionsController.clearDone(); },
+        closeScanner,
+        closeSearch,
+        doSearch,
+        editorController,
+        handleIncomingShare: async () => { await itemsActionsController.handleIncomingShare(); },
+        handleScannedBarcode,
+        loadCategories,
+        loadItems,
+        navigation,
+        openScanner,
+        openSearch,
+        prefetchAdjacentCategories,
+        renderCategoryTabs,
+        renderItems,
+        reorderController,
+        router,
+        savePreferences,
+        scheduleNoteSave,
+        setMessage,
+        setNetworkStatus,
+        setScannerStatus,
+        setUploadProgress,
+        setUserPreferences,
+        swipeController,
+        syncSettingsFrameTheme,
+        tabsViewController,
+        triggerUploadSelectedAttachment: async () => { await itemsActionsController.uploadSelectedAttachment(); },
+        updateFilePickerLabel,
+        updateHeaders,
+    };
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,237 +1,59 @@
-import { appUrl, api, apiUpload } from './api.js';
-import { createAppUiController } from './app-ui.js';
 import { registerAppEventHandlers } from './app-events.js';
 import { initApp, registerServiceWorker, registerUpdateReloadHandler } from './app-init.js';
-import { createHelpersController } from './helpers.js';
-import { createItemsActionsController } from './items-actions.js';
-import { createItemsController } from './items.js';
-import { createItemsViewController } from './items-view.js';
-import { createNavigation } from './navigation.js';
-import { createEditorController } from './editor.js';
-import { createReorderController } from './reorder.js';
-import { createRouter } from './router.js';
-import { createScannerController } from './scanner.js';
-import { createSwipeController } from './swipe.js';
-import { createTabsViewController } from './tabs-view.js';
-import {
-    BARCODE_FORMATS,
-    NOTE_SAVE_DEBOUNCE_MS,
-    SCANNER_COOLDOWN_MS,
-    getCurrentCategory,
-    getCurrentType,
-    getTypeConfig,
-    isBarcodeCategory,
-    isIosWebKit,
-    isNotesCategory,
-    isScannerSupported,
-    normalizePreferences,
-    readInitialPreferences,
-    scannerState,
-    state,
-} from './state.js';
+import { createAppRuntime } from './app-runtime.js';
+import { readInitialPreferences } from './state.js';
 import { applyThemePreferences } from './theme.js';
-import {
-    modeToggleBtns,
-    settingsFrameEl,
-} from './ui.js';
+import { modeToggleBtns } from './ui.js';
 
 let userPreferences = readInitialPreferences();
 let noteSaveTimer = null;
 let tiptapEditor = null;
-let navigation = null;
-let router = null;
-let appUiController = null;
-let helpersController = null;
-let itemsActionsController = null;
-let itemsController = null;
-let itemsViewController = null;
-let scannerController = null;
-let editorController = null;
-let reorderController = null;
-let swipeController = null;
-let tabsViewController = null;
 
 function setUserPreferences(nextPreferences) {
     userPreferences = nextPreferences;
 }
+const runtime = createAppRuntime({
+    getNoteSaveTimer: () => noteSaveTimer,
+    getTiptapEditor: () => tiptapEditor,
+    getUserPreferences: () => userPreferences,
+    setNoteSaveTimer: value => { noteSaveTimer = value; },
+    setTiptapEditor: value => { tiptapEditor = value; },
+    setUserPreferences,
+});
 
-function getItemById(id) { return itemsController.getItemById(id); }
-function getVisibleCategories() { return itemsController.getVisibleCategories(); }
-function cacheCurrentCategoryItems() { return itemsController.cacheCurrentCategoryItems(); }
-function invalidateCategoryCache(categoryId) { return itemsController.invalidateCategoryCache(categoryId); }
-async function loadCategories() { await itemsController.loadCategories(); }
-async function savePreferences(patch) { await itemsController.savePreferences(patch); }
-
-function renderCategoryTabs() { tabsViewController.renderCategoryTabs(); }
-
-function closeScanner() { scannerController.closeScanner(); }
-async function handleScannedBarcode(rawValue) { await scannerController.handleScannedBarcode(rawValue); }
-async function openScanner(action = state.mode === 'einkaufen' ? 'toggle' : 'add') { await scannerController.openScanner(action); }
-
-async function setCategory(categoryId) { await itemsController.setCategory(categoryId); }
-async function loadItems(categoryId = state.categoryId, options = {}) { await itemsController.loadItems(categoryId, options); }
-function prefetchAdjacentCategories() { itemsController.prefetchAdjacentCategories(); }
-function getVisibleItems() { return itemsController.getVisibleItems(); }
-function openSearch() { itemsController.openSearch(); }
-function closeSearch() { itemsController.closeSearch(); }
-async function doSearch(query) { await itemsController.doSearch(query); }
-
-function renderItems() { itemsViewController.renderItems(); }
-
-async function openNoteEditor(item) { await editorController.openNoteEditor(item); }
-async function openNoteEditorWithNavigation(item) { await editorController.openNoteEditorWithNavigation(item); }
-async function closeNoteEditor() { await editorController.closeNoteEditor(); }
-function scheduleNoteSave() { editorController.scheduleNoteSave(); }
-function resetItemForm() { helpersController.resetItemForm(); }
-function syncSettingsFrameTheme() { helpersController.syncSettingsFrameTheme(settingsFrameEl); }
-function triggerHapticFeedback() { helpersController.triggerHapticFeedback(); }
-function isOverdueItem(item) { return helpersController.isOverdueItem(item); }
-function formatDate(value) { return helpersController.formatDate(value); }
-function setMessage(text, isError = false) { appUiController.setMessage(text, isError); }
-function setUploadProgress(fraction) { appUiController.setUploadProgress(fraction); }
-function makeUploadProgressCallback() { return appUiController.makeUploadProgressCallback(); }
-function updateHeaders() { appUiController.updateHeaders(); }
-function updateUploadUi() { appUiController.updateUploadUi(); }
-function updateFilePickerLabel() { appUiController.updateFilePickerLabel(); }
-function setScannerStatus(text, isError = false) { appUiController.setScannerStatus(text, isError); }
-function setNetworkStatus() { appUiController.setNetworkStatus(); }
-function applyTabsVisibility(hidden) { appUiController.applyTabsVisibility(hidden); }
-function formatBytes(sizeBytes) { return appUiController.formatBytes(sizeBytes); }
-async function handleIncomingShare() { await itemsActionsController.handleIncomingShare(); }
-async function uploadSelectedAttachment() { await itemsActionsController.uploadSelectedAttachment(); }
-async function addItem(event) { await itemsActionsController.addItem(event); }
-async function handleToggle(id, done) { await itemsActionsController.handleToggle(id, done); }
-async function handleDelete(id) { await itemsActionsController.handleDelete(id); }
-async function handlePin(id, isPinned) { await itemsActionsController.handlePin(id, isPinned); }
-async function handleEditSave(id) { await itemsActionsController.handleEditSave(id); }
-async function clearDone() { await itemsActionsController.clearDone(); }
-
-router = createRouter({
-    closeNoteEditor,
+const {
+    addItem,
+    applyTabsVisibility,
+    clearDone,
     closeScanner,
     closeSearch,
     doSearch,
-    getItemById,
-    openNoteEditor,
-    openScanner,
-    openSearch,
-    scannerState,
-    setCategory,
-    updateHeaders,
-});
-
-navigation = createNavigation({
-    applyRouteState: router.applyRouteState,
-    getCurrentRouteState: router.getCurrentRouteState,
-});
-
-appUiController = createAppUiController();
-
-helpersController = createHelpersController({
-    getUserPreferences: () => userPreferences,
-    updateFilePickerLabel,
-});
-
-itemsActionsController = createItemsActionsController({
-    cacheCurrentCategoryItems,
-    closeNoteEditor,
-    getItemById,
-    getVisibleCategories,
-    invalidateCategoryCache,
-    loadItems,
-    makeUploadProgressCallback,
-    openNoteEditorWithNavigation,
-    renderItems,
-    resetItemForm,
-    setCategory,
-    setMessage,
-});
-
-tabsViewController = createTabsViewController({
-    getTypeConfig,
-    getVisibleCategories,
-    isTabDragJustFinished: () => reorderController?.wasTabDragJustFinished() ?? false,
-    onCategorySelect: setCategory,
-});
-
-itemsViewController = createItemsViewController({
-    closeSearch,
-    formatBytes,
-    formatDate,
-    getItemById,
-    getVisibleItems,
-    handleDelete,
-    handleEditSave,
-    handlePin,
-    handleToggle,
-    isOverdueItem,
-    openNoteEditorWithNavigation,
-    setCategory,
-});
-
-itemsController = createItemsController({
-    applyTabsVisibility,
-    applyThemePreferences,
-    closeNoteEditor,
-    closeScanner,
-    closeSettings: () => router.closeSettings(),
-    getUserPreferences: () => userPreferences,
-    navigation,
-    normalizePreferences,
-    renderCategoryTabs,
-    renderItems,
-    scannerState,
-    setMessage,
-    setUserPreferences,
-    updateHeaders,
-    updateUploadUi,
-});
-
-scannerController = createScannerController({
-    getCurrentCategory,
-    getItemById,
-    getScannerCooldownMs: () => SCANNER_COOLDOWN_MS,
-    getScannerSupportedFormats: () => BARCODE_FORMATS,
-    handleToggle,
-    invalidateCategoryCache,
-    loadItems,
-    navigation,
-    setMessage,
-    setScannerStatus,
-    triggerHapticFeedback,
-    updateFilePickerLabel,
-});
-
-editorController = createEditorController({
-    cacheCurrentCategoryItems,
-    getItemById,
-    getNoteSaveTimer: () => noteSaveTimer,
-    navigation,
-    setNoteSaveTimer: value => { noteSaveTimer = value; },
-    setTiptapEditor: value => { tiptapEditor = value; },
-    getTiptapEditor: () => tiptapEditor,
-});
-
-reorderController = createReorderController({
-    applyTabsVisibility,
-    cacheCurrentCategoryItems,
-    getItemById,
-    getUserPreferences: () => userPreferences,
-    getVisibleCategories,
-    invalidateCategoryCache,
+    editorController,
+    handleIncomingShare,
+    handleScannedBarcode,
     loadCategories,
     loadItems,
+    navigation,
+    openScanner,
+    openSearch,
+    prefetchAdjacentCategories,
     renderCategoryTabs,
+    renderItems,
+    reorderController,
+    router,
+    savePreferences,
+    scheduleNoteSave,
     setMessage,
-    triggerHapticFeedback,
+    setNetworkStatus,
+    setScannerStatus,
+    setUploadProgress,
+    swipeController,
+    syncSettingsFrameTheme,
+    tabsViewController,
+    triggerUploadSelectedAttachment,
+    updateFilePickerLabel,
     updateHeaders,
-});
-
-swipeController = createSwipeController({
-    getUserPreferences: () => userPreferences,
-    getVisibleCategories,
-    setCategory,
-});
+} = runtime;
 
 registerAppEventHandlers({
     addItem,
@@ -259,7 +81,7 @@ registerAppEventHandlers({
     setUserPreferences,
     syncSettingsFrameTheme,
     tabsViewController,
-    triggerUploadSelectedAttachment: uploadSelectedAttachment,
+    triggerUploadSelectedAttachment,
     updateFilePickerLabel,
     updateHeaders,
     userPreferencesRef: () => userPreferences,
@@ -284,6 +106,6 @@ registerAppEventHandlers({
         updateHeaders,
     });
 
-    await registerServiceWorker('2.0.43');
+    await registerServiceWorker('2.0.44');
     registerUpdateReloadHandler();
 })();
