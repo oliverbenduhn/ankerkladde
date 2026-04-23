dbbf5c14e0cd4715fb756b6e200c510c0c3c6d5b
Oliver Benduhn <oliverbenduhn@gmail.com>
2026-04-23 07:18:23 +0200

    fix: improve category icon visibility in dark themes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 import { startApp } from './app-entry.js';
 
-const assetVersion = new URL(import.meta.url).searchParams.get('v') || '4.2.46';
+const assetVersion = new URL(import.meta.url).searchParams.get('v') || '4.2.47';
 
 startApp(assetVersion);
